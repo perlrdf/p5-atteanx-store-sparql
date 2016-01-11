885863399deb7659262850fde17077961d395459
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-01-11 22:50:58 +0100

    remove the cost code

--- lib/AtteanX/Store/SPARQL/Plan/BGP.pm
+++ lib/AtteanX/Store/SPARQL/Plan/BGP.pm
@@ -60,58 +60,6 @@ sub plan_as_string {
 	return 'SPARQLBGP';
 }
 
-sub cost {
-	# This code is admittedly not very pretty. Advices on how to
-	# simplify are welcome, pull requests even more so
-	my $self = shift;
-	my @kids = @{ $self->children };
-	my $nokids = scalar @kids;
-	my $result = _cost_for_children($nokids);
-	my %variables_in_quads;
-	my $i = 0;
-	foreach my $kid (@kids) {
-		# First create a hash with which quads have which variables
-		my @vars = @{ $kid->in_scope_variables };
-		foreach my $v (@vars) {
-			push(@{$variables_in_quads{$v}}, $i);
-		}
-		$i++;
-	}
-	my %quads_with_joins;
-	my $maxcommon = 0;
-	#$self->log->trace('Variables with indices that occur in quads: ' . Dumper(\%variables_in_quads));
-	foreach my $quads (values(%variables_in_quads)) {
-		my $count = scalar @{$quads};
-		$result -= ($count - 1); # Lower the cost slightly for each shared variable
-		if ($count > 1) {
-			foreach my $quadno (@{$quads}) {
-				$quads_with_joins{$quadno} = 1; # The keys should now be an array with all quads that share a variable
-			}
-		}
-		$maxcommon = $count if ($maxcommon < $count);
-	}
-	return $result if $maxcommon == $nokids; # as many shared variables as quads
-	my @quads_with_joins = sort keys %quads_with_joins;
-	return 50 * $result unless (@quads_with_joins);
-	return 50 * $result if scalar @quads_with_joins < $nokids;
-	# Now look for cartesians
-	my $cartesian = 1; # 1 means no cartesians, provide factor below if it is
-	for (my $j = 0; $j < $nokids; $j++) {
-		if ($j != $quads_with_joins[$j]) {
-			$cartesian = 50;
-			last;
-		}
-	}
-	return $result * $cartesian;
-}
-
-sub has_cost { return 1 }
-
-# A function to estimate a cost of just the number of children
-sub _cost_for_children  {
-	my $nokids = shift;
-	return int(60 + 50*exp(-$nokids/5))
-}
 sub impl {
 	my $self = shift;
 	my $model = shift;
